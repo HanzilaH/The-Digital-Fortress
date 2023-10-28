5e56a34e8c54ba3a05bbe8946b9c3197569eb1d5
Liang Zhang <lz2003@hotmail.co.uk>
2023-10-28 20:15:23 +0100

    To work on

--- src/Flashlight/index.js
+++ src/Flashlight/index.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 import "./index.scss";
 
@@ -6,8 +7,13 @@ const Flashlight = () => {
 
     let [cursorX, setCursorX] = useState(0);
     let [cursorY, setCursorY] = useState(0);
-
-
+    let [animationTimes, setAnimationTimes] = useState({
+        "restartXAnimationTime": (Math.random() + 2) + "s",
+        "nextStageXAnimationTime": (Math.random() + 2) + "s",
+        "restartYAnimationTime": (Math.random() + 3) + "s",
+        "nextStageYAnimationTime": (Math.random() + 3) + "s"
+    });
+    const navigate = useNavigate();
 
     const handleMouseMove = (e) => {
 
@@ -16,21 +22,36 @@ const Flashlight = () => {
 
     }
 
+    const handleOnClick = (e) => {
+
+        navigate(e);
+
+    }
+
     return (
         <div className="flashlight" onMouseMove={(e) => handleMouseMove(e)} style={
+        // <div onMouseMove={(e) => handleMouseMove(e)} style={
             {
                 "--cursorX": cursorX + "px",
                 "--cursorY": cursorY + "px"
             }
         }>
-            <div>
-                <Link to="/">
-                    <button>Restart</button>
-                </Link>
-                <Link to="/test">
-                    <button>Next Stage</button>
-                </Link>
-            </div>
+            <button className="bouncingRestartButton" style={
+                {
+                    "--x-animation-time": animationTimes.restartXAnimationTime,
+                    "--y-animation-time": animationTimes.restartYAnimationTime
+                }
+            } onClick={() => {handleOnClick("/")}}>
+                Restart
+            </button>
+            <button className="bouncingNextStageButton" style={
+                {
+                    "--x-animation-time": animationTimes.nextStageXAnimationTime,
+                    "--y-animation-time": animationTimes.nextStageYAnimationTime
+                }
+            } onClick={() => {handleOnClick("/test")}}>
+                Next Stage
+            </button>
         </div>
     )
 
