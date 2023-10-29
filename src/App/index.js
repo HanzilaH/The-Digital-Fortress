f3f1a6ebac3e44dc33f24fbaca68bb1787a5b1bc
Kristie Phang <kristiepqn@gmail.com>
2023-10-29 02:55:13 +0000

    Implemented reaction time test

--- src/App/index.js
+++ src/App/index.js
@@ -7,6 +7,7 @@ import MathsQuestion from "../MathsQuestion";
 import PuzzleTemplate from "../PuzzleTemplate";
 import FakeCaptcha from "../FakeCaptcha";
 import EndingTiles from "../EndingTiles";
+import ReactionTime from "../ReactionTimeTest";
 
 const routes = [
     {
@@ -24,6 +25,10 @@ const routes = [
     {
         path: "/EndingTiles",
         element: <EndingTiles />
+    },
+    {
+        path: "/reactionTime",
+        element: <ReactionTime />
     }
 ]
 
