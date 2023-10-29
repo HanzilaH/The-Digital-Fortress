d93f88d104820e6d5cb02cba5c266281069c2c45
HanzilaH <120693000+HanzilaH@users.noreply.github.com>
2023-10-29 11:18:32 +0000

    fixed the enter key bug in the first page

--- src/LandingPage/index.js
+++ src/LandingPage/index.js
@@ -1,146 +1,149 @@
-import React from 'react'
-import './index.scss'
-import { useState, useEffect } from 'react';
+import React from "react";
+import "./index.scss";
+import { useState, useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from 'react-router-dom';
-import { SET_INDEX } from '../reducers/indexReducer';
+import { useNavigate } from "react-router-dom";
+import { SET_INDEX } from "../reducers/indexReducer";
 
-const Button = ({onHover}) => {
+const Button = ({ onHover }) => {
+  const [position, setPosition] = useState({
+    x: 48,
+    y: 70,
+  });
 
-    const [position, setPosition] = useState({
-      x: 700,
-      y: 700,
-    });
-
-    const handleClick=()=>{
-        console.log("hiu");
-    }
-
-    const moveButton = () => {
-      const newPosition = {
-        x: Math.floor(Math.random() * 500) + 1,
-        y: Math.floor(Math.random() * 500) + 1,
-      };
-
-      onHover()
-
-      setPosition(newPosition);
-    };
-
-    return (
-      <button
-        id="begin-button"
-        style={{
-            "height": "3em",
-            "width": "5em",
-            "border": "none",
-            "borderRadius": "7em",
-            "background": "#044543",
-            "fontSize": "17px",
-            "color": "#ffffff",
-            "fontFamily": "inherit",
-            "fontWeight": 500,
-            "position": "absolute",
-            "zIndex": 1000,
-          left: position.x + "px",
-          top: position.y + "px",
-        }}
-        onClick={handleClick}
-        onMouseOver={moveButton}
-      >
-        Begin
-      </button>
-    );
+  const handleClick = () => {
+    console.log("hiu");
   };
 
-const GearBox = ()=>{
-    return(
-          <div className="gearbox">
-  <div className="overlay"></div>
-    <div className="gear one">
-      <div className="gear-inner">
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
+  const moveButton = () => {
+    const newPosition = {
+      x: Math.floor(Math.random() * 95) + 1,
+      y: Math.floor(Math.random() * 95) + 1,
+    };
+
+    onHover();
+
+    setPosition(newPosition);
+  };
+
+  return (
+    <button
+      id="begin-button"
+      style={{
+        height: "3em",
+        width: "5em",
+        border: "none",
+        borderRadius: "7em",
+        background: "#044543",
+        fontSize: "17px",
+        color: "#ffffff",
+        fontFamily: "inherit",
+        fontWeight: 500,
+        position: "absolute",
+        zIndex: 1000,
+        left: position.x + "vw",
+        top: position.y + "vh",
+      }}
+      onClick={handleClick}
+      onMouseOver={moveButton}
+    >
+      Begin
+    </button>
+  );
+};
+
+const GearBox = () => {
+  return (
+    <div className="gearbox">
+      <div className="overlay"></div>
+      <div className="gear one">
+        <div className="gear-inner">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+      <div className="gear two">
+        <div className="gear-inner">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+      <div className="gear three">
+        <div className="gear-inner">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
+      </div>
+      <div className="gear four large">
+        <div className="gear-inner">
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+          <div className="bar"></div>
+        </div>
       </div>
     </div>
-    <div className="gear two">
-      <div className="gear-inner">
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-    </div>
-    <div className="gear three">
-      <div className="gear-inner">
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-    </div>
-    <div className="gear four large">
-      <div className="gear-inner">
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-        <div className="bar"></div>
-      </div>
-    </div>
-  </div>
-    )
-}
+  );
+};
 
 const LandingPage = () => {
+  const divRef = useRef(null);
 
-    const dispatch = useDispatch();
-    const FUN_FACTS = useSelector((state) => state.index.FUN_FACTS);
-    const [index, setIndex] = useState(Math.floor(Math.random() * FUN_FACTS.length));
-    dispatch(SET_INDEX(index));
-    const [hoverNumber, setHoverNumber] = useState(0);
-    const [sentence, setSentence] = useState(FUN_FACTS[index]);
-    // const [isShrunk, setIsShrunk] = useState(false);
+  useEffect(() => {
+    divRef.current.focus();
+  }, []);
 
+  const dispatch = useDispatch();
+  const FUN_FACTS = useSelector((state) => state.index.FUN_FACTS);
+  const [index, setIndex] = useState(
+    Math.floor(Math.random() * FUN_FACTS.length)
+  );
+  dispatch(SET_INDEX(index));
+  const [hoverNumber, setHoverNumber] = useState(0);
+  const [sentence, setSentence] = useState(FUN_FACTS[index]);
 
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const handleOnPress = (e) => {
+  const handleOnPress = (e) => {
+    console.log(e.key);
 
-        if (e.key === 'Enter') {
-            navigate("/test2");
-        }
-
+    if (e.key === "Enter") {
+      navigate("/test2");
     }
+  };
 
-    const handleHover =()=>{
-        setHoverNumber(hoverNumber+1)
-        console.log(hoverNumber);
+  const handleHover = () => {
+    setHoverNumber(hoverNumber + 1);
+    console.log(hoverNumber);
 
-        if(hoverNumber>= 10){
-            setSentence("You dummy just press Enter!");
-        }
+    if (hoverNumber >= 10) {
+      setSentence("You dummy just press Enter!");
     }
+  };
   return (
-<div className='landing-page-container' onKeyDown={handleOnPress}>
+    <div
+      className="landing-page-container"
+      ref={divRef}
+      tabIndex={0}
+      onKeyDown={handleOnPress}
+    >
+      <div className="landing-page-title d-flex align-items-center justify-content-center">
+        The Digital Fortress
+      </div>
 
-        <div className='landing-page-title d-flex align-items-center justify-content-center'>
+      <div className="gear-container">
+        <GearBox />
+      </div>
 
-
-            The Digital Fortress
-        </div>
-
-
-    <div className='gear-container'>
-        <GearBox/>
+      <Button onHover={handleHover} />
+      <div className="text-center">{sentence}</div>
     </div>
+  );
+};
 
-        <Button onHover = {handleHover}/>
-        <div className='text-center'>{sentence}</div>
-</div>
-
-
-)
-}
-
-export default LandingPage
+export default LandingPage;
