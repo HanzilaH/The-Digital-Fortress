795b84bd6fb19b055f23cb4c06b04681f01affda
Liang Zhang <lz2003@hotmail.co.uk>
2023-10-29 08:40:01 +0000

    Refactor code

--- src/ReactionTimeTest/index.js
+++ src/ReactionTimeTest/index.js
@@ -1,58 +1,87 @@
 import { useState, useEffect} from "react";
 import PuzzleTemplate from "../PuzzleTemplate";
+import { useNavigate } from "react-router";
+import "./index.scss";
 
 const ReactionTime = () => {
     const [showButton, setShowButton] = useState(false);
     // const [showText, setShowText] = useState(false);
     const [timeLeft, setTimeLeft] = useState(0);
-    const [randomPosition, setRandomPosition] = useState({});
+    // const [randomPosition, setRandomPosition] = useState({
+    //     position: "relative",
+    //     left: `${Math.floor(Math.random() * 60) - 30}vw`,
+    //     top: `${Math.floor(Math.random() * 60) - 30}vh`
+    // });
+    const [randomPosition, setRandomPosition] = useState({
+        "position": "absolute",
+        "top": "150px",
+        "left": "75px"
+    });
+    const [timerStarted, setTimerStarted] = useState(false);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
 
-        setRandomPosition({
-            position: "relative",
-            left: `${Math.floor(Math.random() * 30)}vw`,
-            top: `${Math.floor(Math.random() * 30)}vh`
-        });
+        // setRandomPosition({
+        //     position: "relative",
+        //     left: `${Math.floor(Math.random() * 30)}vw`,
+        //     top: `${Math.floor(Math.random() * 30)}vh`
+        // });
         // Set the countdown and button visibility randomly after 1 to 5 seconds
         const randomTime = Math.floor(Math.random() * (5000 - 1000 + 1)) + 1000;
-        setTimeout(() => {
-          setShowButton(true);
+        const timeout = setTimeout(() => {
+            setShowButton(true);
         //   setShowText(true);
-          setTimeLeft(1);
+            setTimeLeft(1);
+            setTimerStarted(true);
         }, randomTime);
-      }, []);
+
+        return () => {
+
+            clearTimeout(timeout);
+
+        };
+
+    }, []);
 
     useEffect(() => {
         const timer = setTimeout(() => {
-          if (timeLeft > 0) {
-            setTimeLeft(timeLeft - 1);
-          }else {
-            setShowButton(false);
-            // setShowText(false);
-          }
-        }, 1000);
-        return () => clearTimeout(timer);
-      }, [timeLeft]);
+            if (timeLeft > 0) {
+                setTimeLeft(timeLeft - 1);
+            } else {
+                setShowButton(false);
+                // setShowText(false);
+                if (timerStarted) {
 
-    const Click = () => {
-        if(timeLeft>0) {
-            window.location.href = "/"; //link to next page
-        } //else return to landing page
+                    navigate("/");
+
+                }
+            }
+        }, 1000);
+
+        return () => {
+            clearTimeout(timer)
+        };
+    }, [timeLeft]);
+
+    const handleClick = () => {
+
+        navigate("/test"); // link to next page
+
     };
 
     return(
         <PuzzleTemplate title="Think Fast!" puzzle={
             <div>
                 <div className="d-flex justify-content-center">
-                    <div className="template-card" style={{ display: "block"}}>
+                    <div className="template-card">
                         <div style={randomPosition}>
-                        {/* <div> */}
                             {/* {showText && (
                                 <p>Time left: {timeLeft}s</p>
                             )} */}
                             {showButton && (
-                            <button className="button-style" onClick={Click}>
+                            <button className="button-style" onClick={handleClick}>
                                 <span>Click Me!</span>
                             </button>
                             )}
